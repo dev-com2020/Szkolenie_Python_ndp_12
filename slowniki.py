35dc09e656dd328921b410045da5f2f8baea3496
devcom2020 <kwp@tlen.pl>
2022-12-12 15:01:19 +0100

    poniedziałek 15:01

--- slowniki.py
+++ slowniki.py
@@ -1,12 +1,12 @@
 location = {"name": "Kielce",
-        "region": "",
-        "country": "Poland",
-        "lat": 50.83,
-        "lon": 20.67,
-        "tz_id": "Europe/Warsaw",
-        "localtime_epoch": 1670852840,
-        "localtime": "2022-12-12 14:47"}
-
+            "region": ["SW", "MZ", "ZP"],
+            "country": "Poland",
+            "lat": 50.83,
+            "lon": 20.67,
+            "tz_id": "Europe/Warsaw",
+            "localtime_epoch": 1670852840,
+            "zbior": {'jeden': 2022, 'dwa': 2023},
+            "localtime": "2022-12-12 14:47"}
 
 print(location['tz_id'])
 location['name'] = 'Gdynia'
@@ -17,3 +17,5 @@ print(location)
 print(location.keys())
 print(location.values())
 print(location.items())
+print(location['region'][0])
+print(location["zbior"]['dwa'])
