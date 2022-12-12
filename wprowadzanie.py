0952388e1df2819eefbc228c752f0d572b93c012
devcom2020 <kwp@tlen.pl>
2022-12-12 15:35:37 +0100

    poniedziałek 15:35

---
+++ wprowadzanie.py
@@ -0,0 +1,9 @@
+imie = input("Podaj swoje imię: ")
+wiek = input("Podaj swój wiek: ")
+
+print("Twoje imię:", imie.title())
+print(f"Twój wiek: {wiek}")
+print(f"Urodziłeś się w {2022 - int(wiek)} roku")
+
+input('Aby zakończyć program, wciśnij ENTER')
+print("Koniec programu")
