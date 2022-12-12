dada738e70036761624588b6528e81b6decf87de
devcom2020 <kwp@tlen.pl>
2022-12-12 16:01:33 +0100

    poniedziałek 16:00

---
+++ folder/wprowadzanie2.py
@@ -0,0 +1,10 @@
+print("Wprowadzanie2")
+imie = input("Podaj swoje imię: ")
+wiek = input("Podaj swój wiek: ")
+
+print("Twoje imię:", imie.title())
+print(f"Twój wiek: {wiek}")
+print(f"Urodziłeś się w {2022 - int(wiek)} roku")
+
+input('Aby zakończyć program, wciśnij ENTER')
+print("Koniec programu")
---
+++ start.py
@@ -0,0 +1,10 @@
+from folder.wprowadzanie2 import wiek, imie
+
+print("Tutaj plik start...")
+lista = []
+slownik = {}
+lista.append(wiek)
+lista.append(imie)
+slownik[imie] = int(wiek)
+print(lista)
+print(slownik)
