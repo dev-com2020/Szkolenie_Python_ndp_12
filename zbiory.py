fb30605e17ad29a62914441458e3dbe0f8001e71
devcom2020 <kwp@tlen.pl>
2022-12-12 14:40:55 +0100

    poniedziałek 14:40

---
+++ zbiory.py
@@ -0,0 +1,14 @@
+names = {'IBM', 'SPACEX', 'HPE'}
+names.add('IBM')
+names.add('CAT')
+names.add('HPE')
+print(names)
+
+lista = [5, 7, 3, 4, 9, 5, 7, 9]
+lista2 = [5, 7, 3]
+zbior = set(lista)
+zbior2 = set(lista2)
+print(zbior)
+print(zbior2)
+print(zbior.difference(zbior2))
+
