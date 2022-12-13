d122f2b3d9deb91c3e14c1d48ac6763ce8271c00
devcom2020 <kwp@tlen.pl>
2022-12-13 11:41:26 +0100

    wtorek 11:41

---
+++ compr.py
@@ -0,0 +1,17 @@
+# lista = []
+#
+# for numer in range(1999, 2022):
+#     # print(numer)
+#     lista.append(numer)
+#     print(f"Iteracja {numer}, Działam dzięki range")
+#
+# print(lista)
+
+x = [numer for numer in range(1999, 2022)]
+print(x)
+
+slownik = {1: "Azor", 2: "Reksio", 3: "Fafik"}
+print({wartosc: klucz for klucz, wartosc in slownik.items()})
+
+kwadrat = [i ** 2 for i in range(10)]
+print(kwadrat)
