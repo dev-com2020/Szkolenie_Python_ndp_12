c15fe295881d65b3201f93317b4e749f684ad7f4
devcom2020 <kwp@tlen.pl>
2022-12-13 12:10:30 +0100

    wtorek 12:10

--- zadanie.py
+++ zadanie.py
@@ -1,24 +1,26 @@
-liczby = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for i in liczby:
-    print("Wynik kwadratu liczby:", i, " wynosi:", i * i)
+# liczby = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for i in liczby:
+#     print("Wynik kwadratu liczby:", i, " wynosi:", i * i)
+#
+# liczby = []
+# kwadrat = []
+#
+# for i in range(1, 11):
+#     liczby.append(i)
+#     kwadrat.append(i ** 2)
+#     print(i, i ** 2)
+#
+# print(liczby)
+# print(kwadrat)
+#
+# for kwadrat in range(1, 11):
+#     print(kwadrat, kwadrat ** 2)
+#
+# lista = []
+# for k in range(1, 11):
+#     lista.append(k ** 2)
+#
+# print(lista)
 
-
-liczby = []
-kwadrat = []
-
-for i in range(1, 11):
-    liczby.append(i)
-    kwadrat.append(i ** 2)
-    print(i, i ** 2)
-
-print(liczby)
-print(kwadrat)
-
-for kwadrat in range(1, 11):
-    print(kwadrat, kwadrat ** 2)
-
-lista = []
-for k in range(1, 11):
-    lista.append(k ** 2)
-
-print(lista)
+for num in range(5, 0, -1):
+    print(num)
