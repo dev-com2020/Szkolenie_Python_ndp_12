d339463953505e509dae6127fdc921ab201ae9c8
devcom2020 <kwp@tlen.pl>
2022-12-13 15:01:27 +0100

    wtorek 15:01

---
+++ fun1.py
@@ -0,0 +1,16 @@
+def dodawanie(a, b):
+    print(a + b)
+
+
+def odejmowanie(a, b):
+    wynik = a - b
+    return wynik
+
+
+
+lista = []
+lista.append(odejmowanie(40, 22))
+lista.append(odejmowanie(40, 2))
+lista.append(odejmowanie(40, 222))
+# lista.append(dodawanie(23, 33))
+print(lista)
