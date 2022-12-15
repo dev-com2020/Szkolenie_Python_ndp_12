fd0982cdb0a38274f8035bddc447aa7c7124d5e1
devcom2020 <kwp@tlen.pl>
2022-12-15 10:11:48 +0100

    czwartek 10:11

--- jak_dziala_pandas.py
+++ jak_dziala_pandas.py
@@ -1,8 +1,9 @@
 import xml.etree.ElementTree
 import pandas as pd
 
-e = xml.etree.ElementTree.parse(open("./sroda/test.xml"))
-f = pd.DataFrame([{a.get('name'): a.text for a in t} for t in e.findall("tuple")])
-print(f)
+# e = xml.etree.ElementTree.parse(open("./sroda/test.xml"))
+# f = pd.DataFrame([{a.get('name'): a.text for a in t} for t in e.findall("tuple")])
+# print(f)
 # f.to_json('test2.json')
 
+print(pd.read_csv('dane2.csv'))
--- pliki_csv.py
+++ pliki_csv.py
@@ -12,3 +12,15 @@ lista[1][1] = "tomek"
 with open('dane2.csv', 'w', newline="") as file:
     writer = csv.writer(file)
     writer.writerows(lista)
+    writer.writerow([3, 'Arnold', "Warszawa"])
+    writer.writerow([3, 'Hubert'])
+    writer.writerow([3, '', 'Kielce'])
+
+with open('dane3.csv', 'w', newline="") as file:
+    names = ['name', 'score']
+    writer = csv.DictWriter(file, fieldnames=names)
+    writer.writeheader()
+    writer.writerow({'name': 'Tomek', 'score': 4564})
+    writer.writerow({'name': 'Jacek', 'score': 1564})
+    writer.writerow({'name': 'Agata', 'score': 2364})
+
