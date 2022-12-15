a1a99960c9323a79370dd72b7651efb4dfef5b29
devcom2020 <kwp@tlen.pl>
2022-12-15 15:41:23 +0100

    czwartek 15:41

---
+++ problemy.py
@@ -0,0 +1,35 @@
+# def mnozenie(a, b):
+#     try:
+#         return int(a) * int(b)
+#     except TypeError:
+#         return 0
+#     except ValueError:
+#         return 1
+
+# def mnozenie(a, b):
+#     try:
+#         return int(a) * int(b)
+#     except (TypeError, ValueError):
+#         return 0
+
+def mnozenie(a, b):
+    try:
+        wynik = int(a) * int(b)
+    except Exception as e:
+        plik = open('error.log', 'a')
+        plik.write(str(e.args))
+        return 1
+    else:
+        return wynik
+    finally:
+        print(lista)
+
+
+lista = []
+lista.append(mnozenie('20', '34'))
+lista.append(mnozenie(20, 34))
+lista.append(mnozenie(20, None))
+lista.append(mnozenie('a', 'tomek'))
+
+print("Reszta programu...")
+
