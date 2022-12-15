a50df09271e832f2986c22e45cccdc7648ee5724
devcom2020 <kwp@tlen.pl>
2022-12-15 15:03:15 +0100

    czwartek 15:03

---
+++ watki.py
@@ -0,0 +1,30 @@
+from threading import Thread
+from time import sleep, perf_counter
+
+
+def task(id):
+    plik1 = open('t1.txt', 'w')
+    plik1.write(str(id))
+    sleep(1)
+
+def task2(id):
+    plik2 = open('t2.txt', 'w')
+    plik2.write(str(id))
+    sleep(1)
+
+
+# task(1)
+start_time = perf_counter()
+
+t1 = Thread(target=task, args=(1,))
+t2 = Thread(target=task2, args=(2,))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+end_time = perf_counter()
+
+print(f'To zajęło {end_time - start_time:0.2f} sek')
