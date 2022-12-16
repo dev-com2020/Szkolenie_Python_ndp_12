ed19467169a372d35bb2d134767e5341993ae6f9
devcom2020 <kwp@tlen.pl>
2022-12-16 15:06:46 +0100

    piątek 15:06

--- panda/p5.py
+++ panda/p5.py
@@ -1,8 +1,30 @@
 import pandas as pd
 
+
+def zamien(wartosc):
+    if wartosc == 'No':
+        return False
+    if wartosc == 'Yes':
+        return False
+
+
+# films = pd.read_csv('film.csv',
+#                     sep=";",
+#                     encoding="ISO-8859-1",
+#                     skiprows=[1],
+#                     dtype={'Length': 'float64'},
+#                     converters={'Awards': zamien}
+#                     )
+
 films = pd.read_csv('film.csv',
                     sep=";",
                     encoding="ISO-8859-1",
                     skiprows=[1],
+                    dtype={'Length': 'float64'},
+                    converters={'Awards': lambda x: True if x == 'Yes' else False}
                     )
-print(films)
+# print(films.groupby('Year').count())
+# print(films.groupby('Year').Popularity.mean())
+# print(films.groupby(['Year', 'Subject']).Length.mean())
+# print(films.groupby('Year').agg({'Length': ['min', 'max'], 'Popularity': ['min', 'max']}))
+print(films[(films['Year'] >= 1980) & (films['Year'] <= 1990)].groupby('Year').Length.mean())
