dae48638dd9521cc453b246d3cc6722d6e6ddeee
devcom2020 <kwp@tlen.pl>
2022-12-16 14:34:59 +0100

    piątek 14:34

--- panda/p4.py
+++ panda/p4.py
@@ -8,6 +8,22 @@ kostium = pd.read_csv('Halloween.csv', header=2)
 # kostium.to_excel('ttt.xlsx')
 # print(kostium.loc['Arizona'])
 # kostium.loc["Arizona", "1"] = "Batman"
-kostium.loc[3, "1"] = "Batman"
-print(kostium)
-print(kostium.iloc[3, 2])
+# kostium.loc[3, "1"] = "Batman"
+# print(kostium)
+# print(kostium.iloc[3, 2])
+
+# for i, zaw in kostium.iterrows():
+#     if zaw['1'] == 'Rabbit':
+#         print(zaw['region'])
+
+# print(kostium['1'] == 'Rabbit')
+# print(kostium[kostium['1'] == 'Rabbit'])
+# print(kostium[(kostium['1'] == 'Rabbit') | (kostium['1'] == 'Dinosaur')])
+# print(kostium[(kostium['1'] == 'Rabbit') & (kostium['4'] == 'Dinosaur')])
+
+kostium['Nowa'] = "brak"
+kostium.loc[kostium['1'] == 'Doll', 'Nowa'] = "jest"
+# print(kostium.rename(columns={"1": 'Pierwsze_miejsce'}))
+kostium['Złączone'] = kostium['2'] + " / " + kostium['3']
+
+# kostium.to_excel("ttt.xlsx")
