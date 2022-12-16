028a4c4060169fc45361fcd9b9cd0fd6204190f7
devcom2020 <kwp@tlen.pl>
2022-12-16 14:17:11 +0100

    piątek 14:17

--- panda/p4.py
+++ panda/p4.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
 kostium = pd.read_csv('Halloween.csv', header=2)
-print(kostium[:10][['region', '1']])
-print(kostium.index)
-print(kostium.region.is_unique)
-kostium.set_index('region', inplace=True)
-kostium.to_excel('ttt.xlsx')
+# print(kostium[:10][['region', '1']])
+# print(kostium.index)
+# print(kostium.region.is_unique)
+# kostium.set_index('region', inplace=True)
+# kostium.to_excel('ttt.xlsx')
+# print(kostium.loc['Arizona'])
+# kostium.loc["Arizona", "1"] = "Batman"
+kostium.loc[3, "1"] = "Batman"
+print(kostium)
+print(kostium.iloc[3, 2])
